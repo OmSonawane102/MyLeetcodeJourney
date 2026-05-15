209927eca6c221b373f356cc39043554d04b1983
OmSonawane102 <omsonawane1020@gmail.com>
2026-05-16 00:16:47 +0530

    Power(x,n)

---
+++ BinaryExponentiation.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+double pow(int x, int n){
+
+    if(n == 0) return 1.0;
+    if(x == 0) return 0.0;
+    if(x == 1) return 1.0;
+    if(x == -1 && n%2 ==0) return 1.0;
+    if(x == -1 && n%2 ==1) return -1.0;
+    long binaryForm = n;
+    double ans = 1;
+    if(n < 0){
+        x = 1/x;
+        binaryForm = -binaryForm;
+    }
+
+    while(binaryForm > 0 ){
+        if(binaryForm%2 == 1){
+            ans *= x;
+        }
+        x *=x;
+        binaryForm /= 2;
+    }
+    return ans;
+}
+
+int main(){
+
+
+    cout << pow(2, 10) << endl;
+    return 0;
+}
