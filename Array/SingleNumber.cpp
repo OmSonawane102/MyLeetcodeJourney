72a9c214f518791828122163d7030ab0ee2d6cae
OmSonawane102 <omsonawane1020@gmail.com>
2026-07-30 23:01:09 +0530

    Revisited - Single Number

--- Array/SingleNumber.cpp
+++ Array/SingleNumber.cpp
@@ -2,31 +2,6 @@
 #include<vector>
 using namespace std;
 
-
-// here i used nested loops but its not efficient as bit operation : 
-
-/*void singleNumber(vector<int>& nums){
-    int x;
-   for (int i = 0; i < nums.size(); i++){
-        bool isDuplicate = false;
-        for (int j = 0; j < nums.size(); j++){
-            if (i != j && nums[i] == nums[j]){
-                isDuplicate = true;
-                break;
-            }
-            
-        }
-        if(isDuplicate == false){
-            cout << nums[i] << " ";
-
-        }
-        
-   }
-    
-    
-}*/
-
-
 /*BITWISE XOR "^" is used for linear time complexity in single_number problem:
     if two same numbers are apearing in a vector we are trying to cancel out both and the remaining unique number will be our ans ---> lets take 2 as example :
     e will use XOR operator if two appear more than once in a code then it will perform the following operation(binary of 2 is 10);
