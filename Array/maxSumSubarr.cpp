7fa692fd6c7dbfd354a308a893757c073490a847
OmSonawane102 <omsonawane1020@gmail.com>
2026-08-02 21:55:04 +0530

    Revisited - Max Subarray Sum

--- Array/maxSumSubarr.cpp
+++ Array/maxSumSubarr.cpp
@@ -21,23 +21,6 @@ int maxSumOfSubArray(vector<int>& nums){
 }
 
 int main(){
-    /*int n = 5;
-    int arr[5] = {1,4,-5,6,-3};
-    int maxSum = INT_MIN;
-
-    for(int st = 0; st < 5; st++){
-        int currentSum = 0;
-        for(int end = st; end < n; end++){
-            currentSum += arr[end];
-            maxSum = max(currentSum,maxSum);
-        }
-    }
-    cout << "Max sum : " << maxSum << endl;
-    
-    
-    This used brute force approach with O(n^2) second loop just adds new end to current value 
-    max() is in second loop there checks every value and returning max ata last 
-    */
 
     vector<int> arr = {3, -4, 5, 4, -1, 7, -8};
 
