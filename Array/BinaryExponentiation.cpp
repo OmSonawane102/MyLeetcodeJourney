ba6390d01eb445a2d2cc9e9a0ae98f9a76103c4d
Om Sonawane <omsonawane1020@gmail.com>
2026-09-08 20:46:26 +0530

    Revisited - Pow(x, n)

--- Array/BinaryExponentiation.cpp
+++ Array/BinaryExponentiation.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 double pow(int x, int n){
 
+    // for edge cases :
     if(n == 0) return 1.0;
     if(x == 0) return 0.0;
     if(x == 1) return 1.0;
     if(x == -1 && n%2 ==0) return 1.0;
     if(x == -1 && n%2 ==1) return -1.0;
+
+
     long binaryForm = n;
     double ans = 1;
+
     if(n < 0){
         x = 1/x;
         binaryForm = -binaryForm;
     }
 
+    //
     while(binaryForm > 0 ){
         if(binaryForm%2 == 1){
             ans *= x;
