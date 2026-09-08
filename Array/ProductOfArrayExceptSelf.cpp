84c88817fd1167ec10fa125bd815bf03dd01eb72
Om Sonawane <omsonawane1020@gmail.com>
2026-09-08 21:12:44 +0530

    Product Of Array Except Self Brute Force Approach

--- Array/ProductOfArrayExceptSelf.cpp
+++ Array/ProductOfArrayExceptSelf.cpp
@@ -2,12 +2,28 @@
 #include<vector>
 using namespace std;
 
-int productOfArrayExceptSelf(vector<int>& nums){
-    
+vector<int> productOfArrayExceptSelf(vector<int>& nums){
+    int product = 1;
+    vector<int> result;
+    for(int i = 0; i < nums.size(); i++){
+        for(int j = 0; j < nums.size(); j++){
+            if(j == i){
+                continue;
+            }
+            product *= nums[j];
+        }
+        result.push_back(product);
+        product = 1;
+    }
+    return result;
 }
 
 int main(){
 
-
+    vector<int> nums = {1,2,3,4};
+    vector<int> result = productOfArrayExceptSelf(nums);
+    for(int i = 0; i < result.size(); i++){
+        cout << result[i] << " ";
+    }
     return 0;
 }
