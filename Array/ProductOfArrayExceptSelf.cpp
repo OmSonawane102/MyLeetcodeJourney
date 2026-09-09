790cebdaeb161eaa18b5e389248ff500aaae8ddb
Om Sonawane <omsonawane1020@gmail.com>
2026-09-09 23:38:16 +0530

    Optimal Approach - Product Of Array Except Self

--- Array/ProductOfArrayExceptSelf.cpp
+++ Array/ProductOfArrayExceptSelf.cpp
@@ -2,27 +2,22 @@
 #include <vector>
 using namespace std;
 
-// Optimized Approach :
+// Optimized Approach : (with constant O(1) time complexity)
 
 vector<int> productOfArrayExceptSelf(vector<int> &nums){
     int n = nums.size();
     vector<int> ans(n, 1);
-    vector<int> prefix(n, 1);
-    vector<int> suffix(n, 1);
 
-    // Prefix :
+    // prefix -> ans
     for (int i = 1; i < n; i++){
-        prefix[i] = prefix[i - 1] * nums[i - 1];
+        ans[i] = ans[i - 1] * nums[i - 1];
     }
 
-    // Suffix :
+    // Suffix 
+    int suffix = 1;
     for (int i = n-2; i >= 0; i--){
-        suffix[i] = suffix[i+1] * nums[i+1];
-    }
-
-    // Answer :
-    for(int i = 0; i < n; i++){
-        ans[i] = prefix[i] * suffix[i];
+       suffix *= nums[i+1];
+       ans[i] *= suffix; 
     }
 
     return ans;
